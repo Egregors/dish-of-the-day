52c6206146c092339dd4de68a103c36daa56e2bc
Vadim Iskuchekov <vadim.deus@gmail.com>
2016-07-03 20:53:10 +1000

    Загрузка файла со списокм стран. Функция обновления списка. Обновление README.

--- yummy.py
+++ yummy.py
@@ -8,16 +8,49 @@ from __future__ import unicode_literals, absolute_import
     @egregors 2016
 """
 import re
-from urllib import request
+import sys
+from urllib import request, error
 from random import choice
 
-__version__ = '0.2'
+__version__ = '0.3'
 __license__ = "Apache License v2.0"
 
 
 def log(msg=None):
     if msg:
         print('[YUMMY]: {}'.format(msg))
+
+
+def update_countries_list():
+    # Получаем «сырой» список стран с Википедии
+    # Мама, прости меня за это
+    log('Получаю список стран..')
+    log('.')
+    URL = ('https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D1%84%D0%B0%D0%B2'
+           '%D0%B8%D1%82%D0%BD%D1%8B%D0%B9_%D1%81%D0%BF%D0%B8%D1%81%D0%B'
+           'E%D0%BA_%D1%81%D1%82%D1%80%D0%B0%D0%BD_%D0%B8_%D1%82%D0%B5%D'
+           '1%80%D1%80%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%B9?action=raw')
+    try:
+        raw_text = request.urlopen(URL).read().decode('utf-8').split('\n')
+    except error.URLError as e:
+        log('Не удается подключиться к Интернет')
+        log(e)
+        log('Проверьте состояние соединений и попробуйте еще разок')
+        return
+
+    # Стркои с названиями стран начинаются с «#»
+    raw_countries = [x for x in raw_text if x.startswith('#')]
+
+    # Находим названия стран, используя регулярные выражения,
+    # и немного колдунства
+    countries = [x[0][1:-1] for x in [re.findall(
+        '\|[а-яА-Яё ’,—-]+}', k) for k in raw_countries]]
+
+    log('Пишем страны в "./raw/countries.txt"')
+    with open('./raw/countries.txt', mode='wt', encoding='utf-8') as file:
+        file.write('\n'.join(countries))
+    log('.')
+    return countries
 
 
 def get_few_countries():
@@ -32,23 +65,13 @@ def get_few_countries():
     log(cat)
     log('Самое время выбрать страны на эти выходные!')
 
-    # Получаем «сырой» список стран с Википедии
-    # Мама, прости меня за это
-    log('Получаю список стран..')
-    log('.')
-    URL = ('https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D1%84%D0%B0%D0%B2'
-           '%D0%B8%D1%82%D0%BD%D1%8B%D0%B9_%D1%81%D0%BF%D0%B8%D1%81%D0%B'
-           'E%D0%BA_%D1%81%D1%82%D1%80%D0%B0%D0%BD_%D0%B8_%D1%82%D0%B5%D'
-           '1%80%D1%80%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%B9?action=raw')
-    raw_text = request.urlopen(URL).read().decode('utf-8').split('\n')
-
-    # Стркои с названиями стран начинаются с «#»
-    raw_countries = [x for x in raw_text if x.startswith('#')]
-
-    # Находим названия стран, используя регулярные выражения,
-    # и немного колдунства
-    countries = [x[0][1:-1] for x in [re.findall(
-        '\|[а-яА-Яё ’,—-]+}', k) for k in raw_countries]]
+    # Смотрим, есть ли файл со странами
+    try:
+        with open('./raw/countries.txt') as file:
+            countries = file.read().splitlines()
+    except OSError:
+        log('Не найден файл со списокм стран. Пробую скачать..')
+        countries = update_countries_list()
 
     # Выбираем 3 случайные страны
     log('На этой неделе у нас:')
@@ -65,4 +88,7 @@ def get_few_countries():
 
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'update':
+            update_countries_list()
     get_few_countries()
